5f2f55bb0f7823c96b269f10a0d418adead17ab7
akshath <akshath23@icloud.com>
2026-06-01 13:17:46 +0530

    feat: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export const metadata: Metadata = {
 };
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ThemeProvider } from "@/components/chess/ThemeContext";
 
@@ -41,6 +42,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
